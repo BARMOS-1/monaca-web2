5b37d92d4e0784314cdd52b5e646f9d38f941d7e
BARMOS-1 <taiguijitian637@gmail.com>
2026-01-06 12:25:57 +0900

    A

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const MATERIALS = [
 ];
 
 const MASTER_PASSWORD = "1234"; // GAS側と合わせる
-const GAS_URL = "https://script.google.com/macros/s/AKfycbzfnQfTO8ANl-gxaboMnT9HBRGbWPfPj_bbIota51_gsGkjeLRuvPsJQnITxA0XvIS7/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbyyW02JJjwpr7-u_MUb8gEHea9q85wNCRm6IBm6mgBD-vyZVUF8ZDNX4pXPqYUTyJxb/exec";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
